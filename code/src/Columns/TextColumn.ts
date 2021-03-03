2e7066f6c7427ab39237291b9c27285cae3ff644
Ricardo Valim <ricardo.valim@gmail.com>
2021-03-03 11:22:36 +0000

    saving work

--- code/src/Columns/TextColumn.ts
+++ code/src/Columns/TextColumn.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Column {
-    export class TextColumn extends AbstractProviderColumn<ColumnConfig> {
+    export class TextColumn extends AbstractProviderColumn<ColumnConfigText> {
         constructor(grid: Grid.IGrid, columnID: string, configs: JSON) {
-            super(grid, columnID, new ColumnConfig(configs));
+            super(grid, columnID, new ColumnConfigText(configs));
         }
 
         public get columnType(): ColumnType {
