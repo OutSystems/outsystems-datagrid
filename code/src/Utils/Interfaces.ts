3297d724f456a306571e16b5ae52046a7131d678
Ricardo Valim <ricardo.valim@gmail.com>
2021-03-03 13:36:49 +0000

    Update Interfaces.ts

--- code/src/Utils/Interfaces.ts
+++ code/src/Utils/Interfaces.ts
@@ -44,7 +44,7 @@ interface IProviderConfig<T> {
  */
 interface IConfiguration {
     /**
-     * Method responsable for the translation of configuration from OS to Provider
+     * Method responsible for the translation of configuration from OS to Provider
      */
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     getProviderConfig(): any;
