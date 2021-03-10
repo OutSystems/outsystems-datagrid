b3898ab5cfac6bd46b8932a10736260c0e93c1d1
tmbp95 <correio.tiago@gmail.com>
2021-03-10 11:15:50 +0000

    New Cell method inside datagrid API -> SetCellValidation

---
+++ code/src/API/Cells.ts
@@ -0,0 +1,30 @@
+/**
+ * Namespace responsible for all API methods associated to the cells of the Data Grid.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace GridAPI.Cells {
+    /**
+     * Function that validates the cell according to its parameter isValid which comes from OS
+     *
+     * @export
+     * @param {string} gridID ID of the Grid that is to be to check from results.
+     * @param {string} columnIndex Index of the column that contains the cell to be validated.
+     * @param {string} rowIndex Index of the row that contains the cell to be validated.
+     * @param {boolean} isValid State to which the cell should get validated (valid/invalid).
+     * @param {string} errorMessage Message that the cell should show on a tooltip in case of an invalid state.
+     */
+    export function SetCellValidation(
+        gridID: string,
+        rowIndex: number,
+        columnID: string,
+        isValid: boolean,
+        errorMessage: string
+    ): void {
+        GridManager.GetGridById(gridID).features.validationMark.validate(
+            rowIndex,
+            columnID,
+            isValid,
+            errorMessage
+        );
+    }
+}
