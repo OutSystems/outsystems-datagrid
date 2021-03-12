2574fd2eb8345f2361e571e68a0d5344f87052e9
tmbp95 <correio.tiago@gmail.com>
2021-03-12 12:36:42 +0000

    Change name of the methods

--- code/src/API/Cells.ts
+++ code/src/API/Cells.ts
@@ -4,7 +4,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace GridAPI.Cells {
     /**
-     * Function that validates the cell according to its parameter isValid which comes from OS
+     * Responsible for defining a specific cell as valid/invalid and showing an error message to the user when the content of that same cell is invalid.
      *
      * @export
      * @param {string} gridID ID of the Grid that is to be to check from results.
@@ -13,14 +13,14 @@ namespace GridAPI.Cells {
      * @param {boolean} isValid State to which the cell should get validated (valid/invalid).
      * @param {string} errorMessage Message that the cell should show on a tooltip in case of an invalid state.
      */
-    export function SetCellValidation(
+    export function SetValidationStatus(
         gridID: string,
         rowIndex: number,
         columnID: string,
         isValid: boolean,
         errorMessage: string
     ): void {
-        GridManager.GetGridById(gridID).features.validationMark.validate(
+        GridManager.GetGridById(gridID).features.validationMark.setStatus(
             rowIndex,
             columnID,
             isValid,
