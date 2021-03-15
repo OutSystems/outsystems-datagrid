e375e8b1cae636e20d701915c8385999239e2306
tmbp95 <correio.tiago@gmail.com>
2021-03-15 15:54:54 +0000

    Quick fix on comments and add ValidateRow API

--- code/src/API/Cells.ts
+++ code/src/API/Cells.ts
@@ -7,9 +7,9 @@ namespace GridAPI.Cells {
      * Responsible for defining a specific cell as valid/invalid and showing an error message to the user when the content of that same cell is invalid.
      *
      * @export
-     * @param {string} gridID ID of the Grid that is to be to check from results.
-     * @param {string} columnIndex Index of the column that contains the cell to be validated.
-     * @param {string} rowIndex Index of the row that contains the cell to be validated.
+     * @param {string} gridID ID of the Grid.
+     * @param {number} rowIndex Index of the row that contains the cell to be validated.
+     * @param {string} columnID ID of the Column block in which the action of validation should be triggered.
      * @param {boolean} isValid State to which the cell should get validated (valid/invalid).
      * @param {string} errorMessage Message that the cell should show on a tooltip in case of an invalid state.
      */
@@ -25,6 +25,19 @@ namespace GridAPI.Cells {
             columnID,
             isValid,
             errorMessage
+        );
+    }
+
+    /**
+     * Responsible for running the actions responsible for row validation per each column.
+     * Those actions might be included in the OnCellValueChange handler or in case the isMandatory column configuration is set.
+     *
+     * @param {string} gridID ID of the Grid.
+     * @param {number} rowIndex Index of the row that contains the cells to be validated.
+     */
+    export function ValidateRow(gridID: string, rowIndex: number): void {
+        GridManager.GetGridById(gridID).features.validationMark.validateRow(
+            rowIndex
         );
     }
 }
