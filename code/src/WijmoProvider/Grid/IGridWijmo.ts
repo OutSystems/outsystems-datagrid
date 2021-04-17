7b54cac7a4ff1afa92cec72905156da7a1767292
Ricardo Valim <ricardo.valim@gmail.com>
2021-04-17 23:23:33 +0100

    saving work

--- code/src/WijmoProvider/Grid/IGridWijmo.ts
+++ code/src/WijmoProvider/Grid/IGridWijmo.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace WijmoProvider.Grid {
-    export interface IGridWijmo extends IGridGeneric<wijmo.grid.FlexGrid> {}
+    export interface IGridWijmo extends OSFramework.Grid.IGridGeneric<wijmo.grid.FlexGrid> {}
 }
