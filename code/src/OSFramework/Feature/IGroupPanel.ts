a143dcc854ce0c81b6c95a32a60a9c0897e1a34f
Tiago Pereira <tiago.miguel.pereira@outsystems.com>
2021-05-13 17:04:18 +0100

    Update IGroupPanel.ts

--- code/src/OSFramework/Feature/IGroupPanel.ts
+++ code/src/OSFramework/Feature/IGroupPanel.ts
@@ -1,7 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.Feature {
     export interface IGroupPanel extends Interface.IValidation, IView {
+        /** Boolean that indicates whether the grid is grouped or not */
         isGridGrouped: boolean;
+        /**
+         * Check if the column is inside the Group Panel
+         * @param binding binding of the column
+         */
         columnInGroupPanel(binding: string): boolean;
     }
 }
