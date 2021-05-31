661ddf3fff9665ad1250615cca5366d4f38e7885
Gabriel Lundgren <glundgren93@gmail.com>
2021-05-31 11:58:29 +0100

    RGRIDT-831: Fixing eslint on abstractGrid

--- code/src/OSFramework/Grid/AbstractGrid.ts
+++ code/src/OSFramework/Grid/AbstractGrid.ts
@@ -4,52 +4,52 @@ namespace OSFramework.Grid {
         W,
         Z extends Configuration.IConfigurationGrid
     > implements IGridGeneric<W> {
-        private _addedRows: OSFramework.Event.Grid.AddNewRowEvent;
-        private _columns: Map<string, OSFramework.Column.IColumn>;
-        private _columnsGenerator: OSFramework.Column.IColumnGenerator;
-        private _columnsSet: Set<OSFramework.Column.IColumn>;
+        private _addedRows: Event.Grid.AddNewRowEvent;
+        private _columns: Map<string, Column.IColumn>;
+        private _columnsGenerator: Column.IColumnGenerator;
+        private _columnsSet: Set<Column.IColumn>;
         private _configs: Z;
-        private _dataSource: OSFramework.Grid.IDataSource;
-        private _gridEvents: OSFramework.Event.Grid.GridEventsManager;
+        private _dataSource: Grid.IDataSource;
+        private _gridEvents: Event.Grid.GridEventsManager;
         private _isReady: boolean;
         private _uniqueId: string;
-        private _validatingAction: OSFramework.Event.Grid.ValidatingAction;
+        private _validatingAction: Event.Grid.ValidatingAction;
         private _widgetId: string;
 
-        protected _features: OSFramework.Feature.ExposedFeatures;
+        protected _features: Feature.ExposedFeatures;
         protected _provider: W;
+        public abstract autoGenerate: boolean;
 
         constructor(
             uniqueId: string,
             configs: Z,
-            dataSource: OSFramework.Grid.IDataSource,
-            columnsGenerator: OSFramework.Column.IColumnGenerator
+            dataSource: Grid.IDataSource,
+            columnsGenerator: Column.IColumnGenerator
         ) {
             this._uniqueId = uniqueId;
-            this._columns = new Map<string, OSFramework.Column.IColumn>();
-            this._columnsSet = new Set<OSFramework.Column.IColumn>();
+            this._columns = new Map<string, Column.IColumn>();
+            this._columnsSet = new Set<Column.IColumn>();
             this._columnsGenerator = columnsGenerator;
             this._configs = configs;
             this._dataSource = dataSource;
             this._isReady = false;
-            this._addedRows = new OSFramework.Event.Grid.AddNewRowEvent();
-            this._gridEvents = new OSFramework.Event.Grid.GridEventsManager(
-                this
-            );
-            this._validatingAction = new OSFramework.Event.Grid.ValidatingAction();
+            this._addedRows = new Event.Grid.AddNewRowEvent();
+            this._gridEvents = new Event.Grid.GridEventsManager(this);
+            this._validatingAction = new Event.Grid.ValidatingAction();
 
             console.log(`Constructor grid '${this.uniqueId}'`);
         }
 
-        public get validatingAction(): OSFramework.Event.Grid.ValidatingAction {
+        public get validatingAction(): Event.Grid.ValidatingAction {
             return this._validatingAction;
         }
 
-        public get addedRows(): OSFramework.Event.Grid.AddNewRowEvent {
+        public get addedRows(): Event.Grid.AddNewRowEvent {
             return this._addedRows;
         }
 
-        public get dataSource(): OSFramework.Grid.IDataSource {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        public get dataSource(): Grid.IDataSource {
             return this._dataSource;
         }
 
@@ -69,7 +69,7 @@ namespace OSFramework.Grid {
             return this.dataSource.isSingleEntity;
         }
 
-        public get gridEvents(): OSFramework.Event.Grid.GridEventsManager {
+        public get gridEvents(): Event.Grid.GridEventsManager {
             return this._gridEvents;
         }
 
@@ -81,164 +81,9 @@ namespace OSFramework.Grid {
             return this._provider;
         }
 
-        public get features(): OSFramework.Feature.ExposedFeatures {
+        public get features(): Feature.ExposedFeatures {
             return this._features;
         }
-
-        protected finishBuild(): void {
-            this._isReady = true;
-
-            this.gridEvents.trigger(
-                OSFramework.Event.Grid.GridEventType.Initialized,
-                this
-            );
-        }
-
-        public addColumn(col: OSFramework.Column.IColumn): void {
-            console.log(`Add column '${col.uniqueId}': '${col.config.header}'`);
-            this._columns.set(col.config.binding, col);
-            this._columns.set(col.uniqueId, col);
-            this._columnsSet.add(col);
-        }
-
-        public build(): void {
-            //RGRIDT-372 - let's get the ID of the parent element, which will be used by the developer
-            this._widgetId = OSFramework.Helper.GetElementByUniqueId(
-                this.uniqueId
-            ).closest(OSFramework.Helper.Constants.gridTag).id;
-
-            this.dataSource.build();
-        }
-
-        public dispose(): void {
-            this._isReady = false;
-            this._columns.forEach(
-                (col: OSFramework.Column.IColumn, columnID: string) => {
-                    this.removeColumn(columnID);
-                }
-            );
-        }
-
-        public equalsToID(gridID: string): boolean {
-            return gridID === this._uniqueId || gridID === this._widgetId;
-        }
-
-        public getColumn(key: string): OSFramework.Column.IColumn {
-            if (this._columns.has(key)) {
-                return this._columns.get(key);
-            } else {
-                return this.getColumns().find((p) => p && p.equalsToID(key));
-            }
-        }
-
-        public getColumns(): OSFramework.Column.IColumn[] {
-            return Array.from(this._columnsSet);
-        }
-
-        public hasColumn(key: string): boolean {
-            //This method is accessing the map directly, O(1) constant time complexity
-            //Image that this can be used inside a formatItems
-            //Avoid traversing the array here, otherwise we can have a performance degradation
-            return this._columns.has(key);
-        }
-
-        public hasColumnsDefined(): boolean {
-            const widget = OSFramework.Helper.GetElementByUniqueId(
-                this.uniqueId
-            );
-            const gridElement = widget.closest(
-                OSFramework.Helper.Constants.gridTag
-            );
-            const columns = gridElement.querySelectorAll(
-                OSFramework.Helper.Constants.columnCss
-            );
-
-            return columns.length > 0;
-        }
-
-        public hasResults(): boolean {
-            return this.dataSource.hasResults();
-        }
-
-        public removeColumn(columnID: string): void {
-            if (this._columns.has(columnID)) {
-                const col = this._columns.get(columnID);
-
-                col.dispose();
-                this._columns.delete(columnID);
-                this._columns.delete(col.config.binding);
-                this._columnsSet.delete(col);
-
-                console.log(
-                    `Remove column '${columnID}': '${col.config.header}'`
-                );
-            } else {
-                console.error(
-                    `removeColumn - Column id:${columnID} doesn't exist`
-                );
-            }
-        }
-
-        public abstract get rowMetadata(): Interface.IRowMetadata;
-
-        public abstract autoGenerate: boolean;
-
-        public abstract buildFeatures(): void;
-
-        public abstract changeColumnProperty(
-            columnID: string,
-            propertyName: string,
-            // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-            propertyValue: any
-        ): void;
-
-        public abstract changeProperty(
-            propertyName: string,
-            // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-            propertyValue: any
-        ): void;
-
-        public abstract clearAllChanges(): void;
-
-        public abstract getChangesMade(): OSFramework.OSStructure.ChangesDone;
-
-        // public abstract getData(): JSON[];
-
-        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-        public abstract getViewLayout(): any;
-
-        public abstract setCellError(
-            binding: string,
-            row: number,
-            message: string
-        ): void;
-
-        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-        // public abstract setData(data: any): boolean;
-
-        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-        public abstract setViewLayout(state: any): void;
-
-        public getData(): JSON[] {
-            return this.dataSource.getData();
-        }
-
-        public setData(data: string): boolean {
-            this.dataSource.setData(data);
-
-            if (this.isReady) {
-                if (!this.hasColumnsDefined()) {
-                    this._autoGenCol();
-                } else {
-                    this._validateBindings();
-                }
-
-                return true;
-            }
-
-            return false;
-        }
-
         private _autoGenCol(): void {
             //let's auto generate the columns
             if (this.dataSource.hasMetadata) {
@@ -285,5 +130,146 @@ namespace OSFramework.Grid {
                 });
             }
         }
+        protected finishBuild(): void {
+            this._isReady = true;
+
+            this.gridEvents.trigger(Event.Grid.GridEventType.Initialized, this);
+        }
+
+        public addColumn(col: Column.IColumn): void {
+            console.log(`Add column '${col.uniqueId}': '${col.config.header}'`);
+            this._columns.set(col.config.binding, col);
+            this._columns.set(col.uniqueId, col);
+            this._columnsSet.add(col);
+        }
+
+        public build(): void {
+            //RGRIDT-372 - let's get the ID of the parent element, which will be used by the developer
+            this._widgetId = Helper.GetElementByUniqueId(this.uniqueId).closest(
+                Helper.Constants.gridTag
+            ).id;
+
+            this.dataSource.build();
+        }
+
+        public dispose(): void {
+            this._isReady = false;
+            this._columns.forEach((col: Column.IColumn, columnID: string) => {
+                this.removeColumn(columnID);
+            });
+        }
+
+        public equalsToID(gridID: string): boolean {
+            return gridID === this._uniqueId || gridID === this._widgetId;
+        }
+
+        public getColumn(key: string): Column.IColumn {
+            if (this._columns.has(key)) {
+                return this._columns.get(key);
+            } else {
+                return this.getColumns().find((p) => p && p.equalsToID(key));
+            }
+        }
+
+        public getColumns(): Column.IColumn[] {
+            return Array.from(this._columnsSet);
+        }
+
+        public getData(): JSON[] {
+            return this.dataSource.getData();
+        }
+
+        public hasColumn(key: string): boolean {
+            //This method is accessing the map directly, O(1) constant time complexity
+            //Image that this can be used inside a formatItems
+            //Avoid traversing the array here, otherwise we can have a performance degradation
+            return this._columns.has(key);
+        }
+
+        public hasColumnsDefined(): boolean {
+            const widget = Helper.GetElementByUniqueId(this.uniqueId);
+            const gridElement = widget.closest(Helper.Constants.gridTag);
+            const columns = gridElement.querySelectorAll(
+                Helper.Constants.columnCss
+            );
+
+            return columns.length > 0;
+        }
+
+        public hasResults(): boolean {
+            return this.dataSource.hasResults();
+        }
+
+        public removeColumn(columnID: string): void {
+            if (this._columns.has(columnID)) {
+                const col = this._columns.get(columnID);
+
+                col.dispose();
+                this._columns.delete(columnID);
+                this._columns.delete(col.config.binding);
+                this._columnsSet.delete(col);
+
+                console.log(
+                    `Remove column '${columnID}': '${col.config.header}'`
+                );
+            } else {
+                console.error(
+                    `removeColumn - Column id:${columnID} doesn't exist`
+                );
+            }
+        }
+
+        public setData(data: string): boolean {
+            this.dataSource.setData(data);
+
+            if (this.isReady) {
+                if (!this.hasColumnsDefined()) {
+                    this._autoGenCol();
+                } else {
+                    this._validateBindings();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+        public abstract get rowMetadata(): Interface.IRowMetadata;
+
+        public abstract buildFeatures(): void;
+
+        public abstract changeColumnProperty(
+            columnID: string,
+            propertyName: string,
+            // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+            propertyValue: any
+        ): void;
+
+        public abstract changeProperty(
+            propertyName: string,
+            // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+            propertyValue: any
+        ): void;
+
+        public abstract clearAllChanges(): void;
+
+        public abstract getChangesMade(): OSStructure.ChangesDone;
+
+        // public abstract getData(): JSON[];
+
+        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+        public abstract getViewLayout(): any;
+
+        public abstract setCellError(
+            binding: string,
+            row: number,
+            message: string
+        ): void;
+
+        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+        // public abstract setData(data: any): boolean;
+
+        // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+        public abstract setViewLayout(state: any): void;
     }
 }
