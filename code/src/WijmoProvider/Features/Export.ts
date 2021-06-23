daf8b1cbb17b8050dd60998ebd040729125b949c
techrug <rmfgoncalves@gmail.com>
2021-06-23 18:46:21 +0300

    Adding the ability to export with a specific file name.

--- code/src/WijmoProvider/Features/Export.ts
+++ code/src/WijmoProvider/Features/Export.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace WijmoProvider.Feature {
     export class Export
-        implements OSFramework.Feature.IExport, OSFramework.Interface.IBuilder {
+        implements OSFramework.Feature.IExport, OSFramework.Interface.IBuilder
+    {
         private _curPage: number;
         private _grid: WijmoProvider.Grid.IGridWijmo;
         private _pageSize: number;
@@ -66,10 +67,10 @@ namespace WijmoProvider.Feature {
             wijmo.Clipboard.copy(result);
         }
 
-        public exportToCsv(): void {
+        public exportToCsv(filename = 'DataGridReactive'): void {
             this._resetPagination();
 
-            const params = { fileName: 'DataGridReactive.csv' };
+            const params = { fileName: `${filename}.csv` };
             const result = this._grid.provider.getClipString(
                 this._getFullCellRange(),
                 true,
@@ -80,7 +81,10 @@ namespace WijmoProvider.Feature {
             wijmo.saveFile(result, params.fileName);
         }
 
-        public exportToExcel(withStyles: boolean): void {
+        public exportToExcel(
+            withStyles: boolean,
+            filename = 'DataGridReactive'
+        ): void {
             this._resetPagination();
             // include timeout in order to apply conditional format
             setTimeout(() => {
@@ -95,7 +99,7 @@ namespace WijmoProvider.Feature {
                     params
                 );
                 book.sheets[0].name = 'DataGrid Data';
-                book.save('DataGridReactive.xlsx');
+                book.save(`${filename}.xlsx`);
                 this._reApplyPagination();
             }, 10);
         }
