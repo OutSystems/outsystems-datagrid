3ea2d344bcdfb1a09e287a9296749a32399ba85c
João Ferreira <joao.manuel.ferreira@outsystems.com>
2021-06-24 16:10:08 +0100

    - document public method;

--- code/src/OSFramework/Feature/IDirtyMark.ts
+++ code/src/OSFramework/Feature/IDirtyMark.ts
@@ -3,6 +3,10 @@ namespace OSFramework.Feature {
     export interface IDirtyMark {
         clear(): void;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        /**
+         * Clears dirty marks in the given row.
+         * @param {*} row
+         */
         clearPropertyInRow(row: any): void;
         getOldValue(rowNumber: number, binding: string): any;
     }
