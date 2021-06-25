87ed21a68b892aa5a5d067f12b972911a1f83555
João Ferreira <joao.manuel.ferreira@outsystems.com>
2021-06-25 19:32:30 +0100

    - document new methods;

--- code/src/GridAPI/Styling.ts
+++ code/src/GridAPI/Styling.ts
@@ -3,6 +3,15 @@
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace GridAPI.Styling {
+    /**
+     * Function that will add a specific CSS class to a cell
+     *
+     * @export
+     * @param {string} gridID
+     * @param {string} columnID
+     * @param {number} rowIndex
+     * @param {string} className
+     */
     export function SetCellCssClass(
         gridID: string,
         columnID: string,
@@ -16,7 +25,16 @@ namespace GridAPI.Styling {
             className
         );
     }
-
+    /**
+     * Function that will add a specific CSS class to the cells of a column.
+     * When applyToHeader is true this will affect the header cell, otherwise will just affect the body
+     *
+     * @export
+     * @param {string} gridID
+     * @param {string} columnID
+     * @param {string} cssClass
+     * @param {boolean} applyToHeader
+     */
     export function SetColumnCssClass(
         gridID: string,
         columnID: string,
@@ -35,7 +53,14 @@ namespace GridAPI.Styling {
             );
         }
     }
-
+    /**
+     * Function that will remove all the CSS classes that were added to a Cell.
+     *
+     * @export
+     * @param {string} gridID
+     * @param {string} columnID
+     * @param {number} rowIndex
+     */
     export function RemoveAllCssClassesFromCell(
         gridID: string,
         columnID: string,
@@ -47,7 +72,14 @@ namespace GridAPI.Styling {
             binding
         );
     }
-
+    /**
+     * Function that will remove a added CSS class from a column.
+     *
+     * @export
+     * @param {string} gridID
+     * @param {string} columnID
+     * @param {string} cssClass
+     */
     export function RemoveColumnCssClass(
         gridID: string,
         columnID: string,
--- code/src/OSFramework/Feature/IStyling.ts
+++ code/src/OSFramework/Feature/IStyling.ts
@@ -2,26 +2,26 @@
 namespace OSFramework.Feature {
     export interface IStyling {
         /**
-         *
+         * Add a specific CSS Class to the content of a column (header).
          * @param {string} columnID
          * @param {string} className
          */
         addCssClass(columnID: string, className: string): void;
         /**
-         *
+         * Add a specific CSS Class to a column (header and content).
          * @param {string} columnID
          * @param {string} className
          */
         addCssClassAll(columnID: string, className: string): void;
         changeRowHeight(rowHeight: number): void;
         /**
-         *
+         * Removes a specific CSS Class from a column content
          * @param {string} columnID
          * @param {string} className
          */
         removeCssClass(columnID: string, className: string): void;
         /**
-         *
+         * Removes a specific CSS Class from an entire column
          * @param {string} columnID
          * @param {string} className
          */
