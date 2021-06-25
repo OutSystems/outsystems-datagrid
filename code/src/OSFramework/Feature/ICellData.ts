05a5638d3c4f132e5013e9ff542440999089b19f
bmartinho <bruno.martinho@outsystems.com>
2021-06-25 15:10:54 +0100

    Changed columnID to column in ICellData

--- code/src/OSFramework/Feature/ICellData.ts
+++ code/src/OSFramework/Feature/ICellData.ts
@@ -5,12 +5,12 @@ namespace OSFramework.Feature {
          * Responsible for updating a specific cell -
          * This is needed in a case we wnat to update another column cell, for example when a cell content is denpendent on another.
          * @param {number} rowNumber
-         * @param {string} columnID
+         * @param {string} column
          * @param {string} value
          */
         setCellData(
             rowNumber: number,
-            columnID: OSFramework.Column.IColumn,
+            column: OSFramework.Column.IColumn,
             // eslint-disable-next-line
             value: string
         ): void;
