e062267d4c73d2a9b40876c1850162bc1d40034a
rug <rmfgoncalves@gmail.com>
2021-09-16 15:28:59 +0300

    Migrating to NETstandard

--- code/extension/DataGridDataConvert/Source/NET/Interface.cs
+++ code/extension/DataGridDataConvert/Source/NET/Interface.cs
@@ -8,14 +8,6 @@ namespace OutSystems.NssDataGridDataConvert {
 	public interface IssDataGridDataConvert {
 
 		/// <summary>
-		/// DEPRECATED - c
-		/// </summary>
-		/// <param name="ssData">List or record to get meta data from.</param>
-		/// <param name="ssDataJSON">JSON with the data for the data grid</param>
-		/// <param name="ssDataMetadata">JSON with the structure of the object passed.</param>
-		void MssConvertData2JSON_deprecated(object ssData, out string ssDataJSON, out string ssDataMetadata);
-
-		/// <summary>
 		/// Prepares your data to be used in the Data Grid.
 		/// </summary>
 		/// <param name="ssData">List or record to get meta data from.</param>
