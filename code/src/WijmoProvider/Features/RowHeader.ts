b7a21a5cd969f2c943a553829e358845458fe980
Gabriel Lundgren <glundgren93@gmail.com>
2021-10-28 16:47:47 +0100

    RGRIDT-1037: Disabling row number selection

--- code/src/WijmoProvider/Features/RowHeader.ts
+++ code/src/WijmoProvider/Features/RowHeader.ts
@@ -12,6 +12,7 @@ namespace WijmoProvider.Feature {
             this._grid = grid;
             this._rowHeaderType = rowHeaderType;
         }
+
         public get type(): OSFramework.Enum.RowHeader {
             return this._rowHeaderType;
         }
@@ -39,18 +40,48 @@ namespace WijmoProvider.Feature {
             column.allowSorting = false;
             column.allowDragging = false;
             column.allowMerging = false;
-            this._grid.provider.rowHeaders.columns.push(column);
 
-            new wijmo.grid.selector.Selector(column);
+            new wijmo.grid.selector.Selector(this._grid.provider);
+            this._grid.provider.rowHeaders.columns.insert(0, column);
         }
 
         private _buildRowHeader() {
             if (this.hasCheckbox) {
                 this._buildCheckbox();
+                this._disableRowSelectionOnRowNumber();
+
+                this._grid.provider.selectionMode =
+                    wijmo.grid.SelectionMode.CellRange;
+
+                // check if Grid has checked rows
+                this._grid.provider.formatItem.addHandler((s) => {
+                    const element =
+                        s.columnHeaders.hostElement.querySelector(
+                            '.wj-cell.wj-header'
+                        );
+
+                    const el = s.hostElement.querySelector(
+                        ".wj-header input[type='checkbox']:checked"
+                    );
+                    if (el) {
+                        wijmo.addClass(element, 'checked-columns');
+                    } else if (wijmo.hasClass(element, 'checked-columns')) {
+                        wijmo.removeClass(element, 'checked-columns');
+                    }
+                });
             }
             if (!this.hasRowNumber) {
                 this._grid.features.autoRowNumber.setState(false);
             }
+        }
+
+        /**
+         * Disable row number header from being clicked as we want to select rows only with checkbox
+         */
+        private _disableRowSelectionOnRowNumber(): void {
+            this._grid.provider.hostElement
+                .querySelector('.wj-rowheaders')
+                .addEventListener('mousedown', (e) => e.preventDefault());
         }
 
         public build(): void {
