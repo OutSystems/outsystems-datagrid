49c7ec457d3cf6c5afa8f90050984d968d2756e9
Gabriel Lundgren <glundgren93@gmail.com>
2021-11-03 14:15:32 +0000

    RGRIDT-1039: Adjusting getProviderAllSelections method to remove intersection

--- code/src/WijmoProvider/Features/RowHeader.ts
+++ code/src/WijmoProvider/Features/RowHeader.ts
@@ -52,7 +52,7 @@ namespace WijmoProvider.Feature {
                 // want to still be able to select cells.
                 this._disableRowSelectionOnRowNumber();
                 this._grid.provider.selectionMode =
-                    wijmo.grid.SelectionMode.CellRange;
+                    wijmo.grid.SelectionMode.MultiRange;
 
                 // if grid has checked rows, add custom class so column headers are selected as well
                 this._grid.provider.formatItem.addHandler((s, e) => {
