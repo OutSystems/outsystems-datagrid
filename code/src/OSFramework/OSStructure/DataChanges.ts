11cdc72f23fbb78dda20ce35ba7cbf40a65f2ff7
Gabriel Lundgren <glundgren93@gmail.com>
2021-12-16 09:53:52 +0000

    RGRIDT-1098: fixed

--- code/src/OSFramework/OSStructure/DataChanges.ts
+++ code/src/OSFramework/OSStructure/DataChanges.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.OSStructure {
     export class DataChanges {
-        public changedRows: any;
+        public changedRows: unknown;
         public totalRows: number;
     }
 }
