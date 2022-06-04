522d8f605401cfa8561b3c2785286f6d024a2376
gbnm <goncalo.martins@outsystems.com>
2022-06-04 14:14:13 +0100

    Added versioning
    Added versioning and GetVersion method

---
+++ code/src/OSFramework/Constants.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace OSFramework.Constants {
+    /* OutSystems Data Grid Version */
+    export const OSDataGridVersion = '2.8.0';
+}
