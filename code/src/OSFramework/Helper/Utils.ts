41a265f164fbdbe9923188cf473873b9149f823e
Gabriel Lundgren <gabriel.lundgren@outsystems.com>
2022-08-08 12:47:23 +0200

    ROU-3605: removing unused code

--- code/src/OSFramework/Helper/Utils.ts
+++ code/src/OSFramework/Helper/Utils.ts
@@ -1,20 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace OSFramework.Helper {
-    function getChunks(quantity, size) {
-        const splitQuantity = Math.round(quantity / size);
-        const remainder = quantity % size;
-        let result = [];
-        for (let index = 0; index < size; index++) {
-            if (index === 0 && remainder > 0) {
-                result = [...result, splitQuantity + remainder];
-            } else {
-                result = [...result, splitQuantity];
-            }
-        }
-
-        return result;
-    }
-
     export function BatchArray(
         data: Array<string | number | JSON>,
         callback: (part) => void,
@@ -25,13 +10,5 @@ namespace OSFramework.Helper {
         const chunk = _.chunk(data, chunkSize);
 
         chunk.forEach((part) => callback(part));
-    }
-
-    export function Batch(quantity, callback, chunkSize = 10): void {
-        const chunks = getChunks(quantity, chunkSize);
-
-        chunks.forEach((chunk) => {
-            callback(chunk);
-        });
     }
 }
