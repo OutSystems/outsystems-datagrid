f518e5c634c9a94f8644cf9a3f2cea37fd8d9c23
giuliana <giuliana.silva@outsystems.com>
2022-09-21 09:26:11 +0100

    Change to get column from grid

--- src/OutSystems/GridAPI/Cell.ts
+++ src/OutSystems/GridAPI/Cell.ts
@@ -60,9 +60,10 @@ namespace OutSystems.GridAPI.Cells {
     ): void {
         Performance.SetMark('Cells.validateCell');
 
-        const column = ColumnManager.GetColumnById(columnID);
+        const grid = GridManager.GetGridById(gridID);
+        const column = grid.getColumn(columnID);
         if (column === undefined) return;
-        GridManager.GetGridById(gridID).features.validationMark.validateCell(
+        grid.features.validationMark.validateCell(
             rowIndex,
             column,
             triggerOnCellValueChange
