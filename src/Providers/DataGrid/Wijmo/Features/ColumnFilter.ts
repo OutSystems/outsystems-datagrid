91a6da172f0a386a12226df67af774bba1a54040
rug <rmfgoncalves@gmail.com>
2023-11-28 13:07:44 +0100

    Adding columns allowed for setColumnFilterOptions method

--- src/Providers/DataGrid/Wijmo/Features/ColumnFilter.ts
+++ src/Providers/DataGrid/Wijmo/Features/ColumnFilter.ts
@@ -349,7 +349,17 @@ namespace Providers.DataGrid.Wijmo.Feature {
                     column.columnType ===
                         OSFramework.DataGrid.Enum.ColumnType.Text ||
                     column.columnType ===
-                        OSFramework.DataGrid.Enum.ColumnType.Dropdown
+                        OSFramework.DataGrid.Enum.ColumnType.Dropdown ||
+                    column.columnType ===
+                        OSFramework.DataGrid.Enum.ColumnType.Checkbox ||
+                    column.columnType ===
+                        OSFramework.DataGrid.Enum.ColumnType.Currency ||
+                    column.columnType ===
+                        OSFramework.DataGrid.Enum.ColumnType.Date ||
+                    column.columnType ===
+                        OSFramework.DataGrid.Enum.ColumnType.DateTime ||
+                    column.columnType ===
+                        OSFramework.DataGrid.Enum.ColumnType.Number
                 ) {
                     // this column will have both filter types
                     this.changeFilterType(
