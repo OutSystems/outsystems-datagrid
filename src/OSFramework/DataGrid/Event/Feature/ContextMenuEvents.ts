b18f2e505c1b49985d4240dc9be4ea7b5613bf76
rug <rmfgoncalves@gmail.com>
2024-06-13 13:06:30 +0200

    Adding new event

--- src/OSFramework/DataGrid/Event/Feature/ContextMenuEvents.ts
+++ src/OSFramework/DataGrid/Event/Feature/ContextMenuEvents.ts
@@ -7,16 +7,23 @@ namespace OSFramework.DataGrid.Event.Feature {
 	 * @extends {OSFramework.DataGrid.Event.AbstractEvent<string>}
 	 */
 	export class ToggleContextMenu extends Event.AbstractEvent<string> {
-		public trigger(gridID: string, isOpening: boolean, columnId: string): void {
-			this.handlers.slice(0).forEach((h) => {
-				if (isOpening) {
-					//only when opening the context menu, we need the operation
-					//to be synchronous, so that the items can be updated.
-					h(gridID, isOpening, columnId);
-				} else {
-					Helper.AsyncInvocation(h, gridID, isOpening, columnId);
-				}
-			});
+		public trigger(gridID: string, columnId: string, isOpening: boolean): void {
+			this.handlers.slice(0).forEach((h) => Helper.AsyncInvocation(h, gridID, isOpening, columnId));
+		}
+	}
+
+	/**
+	 * Class that encapsulate the basic logic of triggering the event of opening the context menu.
+	 * This event is synchronous to allow the developer to change the Context Menu items before the
+	 * menu is visibel. The event be fired before the context menu is opened.
+	 *
+	 * @export
+	 * @class OpeningContextMenu
+	 * @extends {Event.AbstractEvent<string>}
+	 */
+	export class OpeningContextMenu extends Event.AbstractEvent<string> {
+		public trigger(gridID: string, columnId: string): void {
+			this.handlers.slice(0).forEach((h) => h(gridID, columnId));
 		}
 	}
 }
