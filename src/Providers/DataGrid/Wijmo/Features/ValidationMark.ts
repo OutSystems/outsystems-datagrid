fe696c6bc329e7dbbdb7012018dcc5d48b44061d
Giuliana Silva <108938618+OS-giulianasilva@users.noreply.github.com>
2024-07-31 14:15:48 +0100

    Remove OnCellValueChange trigger from ValidateRow API (#426)

--- src/Providers/DataGrid/Wijmo/Features/ValidationMark.ts
+++ src/Providers/DataGrid/Wijmo/Features/ValidationMark.ts
@@ -633,8 +633,7 @@ namespace Providers.DataGrid.Wijmo.Feature {
 					// This method gets executed by an API. No values change in columns, so the current value and the original one (old value) are the same.
 					const currValue = this._grid.provider.getCellData(rowNumber, column.provider.index, false);
 
-					// Triggers the events of OnCellValueChange associated to a specific column in OS
-					this._triggerEventsFromColumn(rowNumber, column.uniqueId, currValue, currValue);
+					this._setCellStatus(column, rowNumber, currValue);
 				}
 			});
 		}
