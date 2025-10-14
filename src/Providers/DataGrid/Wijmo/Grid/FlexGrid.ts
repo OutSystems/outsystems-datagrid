5ae20908be1c5da8c9c9327e747ce7003685a989
Giuliana Silva <giuliana.silva@outsystems.com>
2025-10-14 18:10:17 +0100

    remove workaround for WJM-36680

--- src/Providers/DataGrid/Wijmo/Grid/FlexGrid.ts
+++ src/Providers/DataGrid/Wijmo/Grid/FlexGrid.ts
@@ -62,9 +62,6 @@ namespace Providers.DataGrid.Wijmo.Grid {
 				this._provider.itemsSource.itemsEdited.remove(row.dataItem);
 				this._provider.itemsSource.itemsRemoved.remove(row.dataItem);
 				this._provider.itemsSource.itemsAdded.remove(row.dataItem);
-				// RUG: Workaround provided by Mescius, July 2025
-				// Fixes the issue described in ROU-12063.
-				this._provider.itemsSource._orgVals?.delete(row.dataItem);
 			});
 		}
 
