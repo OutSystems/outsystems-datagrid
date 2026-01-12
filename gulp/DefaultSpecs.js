1e5af231993750f7392fb3fbbc44d7b5a7d3e1b1
Giuliana Silva <giuliana.silva@outsystems.com>
2026-01-12 17:50:38 +0000

    Update version to 2.21.1 in package.json, DefaultSpecs.js, and Constants.ts

--- gulp/DefaultSpecs.js
+++ gulp/DefaultSpecs.js
@@ -19,7 +19,7 @@ const constants = {
 
 // Store the default project specifications
 const specs = {
-	version: '2.21.0',
+	version: '2.21.1',
 	name: 'OutSystems DataGrid',
 	description: '',
 	url: 'Website:\n • ' + constants.websiteUrl,
