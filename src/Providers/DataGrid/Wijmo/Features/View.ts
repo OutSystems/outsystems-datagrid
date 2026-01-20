9f62f7346d57877dfab33632ce591c6455e7ea0f
Giuliana Silva <giuliana.silva@outsystems.com>
2026-01-20 17:56:37 +0000

    fix(view): fix issue column not visible when using setViewLayout

--- src/Providers/DataGrid/Wijmo/Features/View.ts
+++ src/Providers/DataGrid/Wijmo/Features/View.ts
@@ -136,12 +136,12 @@ namespace Providers.DataGrid.Wijmo.Feature {
 
 			const config = JSON.parse(state);
 			this._grid.provider.deferUpdate(() => {
-				this._reloadColumns(config);
 				this._grid.features.filter.setViewLayout(config);
 				this._grid.features.groupPanel.setViewLayout(config);
 				this._grid.features.sort.setViewLayout(config);
 				this._grid.features.columnFreeze.setViewLayout(config);
 				this._setGroups(this._grid.provider.columnGroups, config.groupColumns);
+				this._reloadColumns(config);
 			});
 		}
 	}
