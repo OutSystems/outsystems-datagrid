eafee27d87fe8f07690dabeca54917fc0ecd2f9c
Giuliana Silva <108938618+OS-giulianasilva@users.noreply.github.com>
2026-01-22 10:13:47 +0000

    ROU-12515: Saved View filters don't show in UI since version 2.21.0 (#484)
    * fix(layout, filter): fix issue with filter not being visually applied when usign SetViewLayout
    
    * fix(view): fix issue column not visible when using setViewLayout
    
    * chore(): update version to 2.21.2

--- gulp/DefaultSpecs.js
+++ gulp/DefaultSpecs.js
@@ -19,7 +19,7 @@ const constants = {
 
 // Store the default project specifications
 const specs = {
-	version: '2.21.1',
+	version: '2.21.2',
 	name: 'OutSystems DataGrid',
 	description: '',
 	url: 'Website:\n • ' + constants.websiteUrl,
