f6b791d3efd27e7b6b611153aacce03e009042d1
rug <rmfgoncalves@gmail.com>
2026-02-19 12:55:59 +0100

    Removing unneeded dependencies

--- extension/DataGridUtils/Source/NET/Interface.cs
+++ extension/DataGridUtils/Source/NET/Interface.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Data;
 using OutSystems.HubEdition.RuntimePlatform;
 
 namespace OutSystems.NssDataGridUtils {
--- extension/DataGridUtils/Source/NET/temp_ardoJSON.cs
+++ extension/DataGridUtils/Source/NET/temp_ardoJSON.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,7 +6,6 @@ using System.Reflection;
 using System.Text;
 using Newtonsoft.Json;
 using OutSystems.HubEdition.RuntimePlatform;
-using OutSystems.HubEdition.RuntimePlatform.Db;
 
 namespace OutSystems.NssDataGridUtils {
     public class temp_ardoJSON {
