1da710e781b8e44cac1144ce5f22e52727a476ae
rug <rmfgoncalves@gmail.com>
2026-02-19 11:59:37 +0100

    Make recCache dictionary thread safe

--- extension/DataGridUtils/Source/NET/temp_ardoJSON.cs
+++ extension/DataGridUtils/Source/NET/temp_ardoJSON.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,7 +12,7 @@ using OutSystems.HubEdition.RuntimePlatform.Db;
 namespace OutSystems.NssDataGridUtils {
     public class temp_ardoJSON {
         private static DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        private static Dictionary<Type, Dictionary<string, FieldHolder>> recCache = new Dictionary<Type, Dictionary<string, FieldHolder>>();
+        private static ConcurrentDictionary<Type, Dictionary<string, FieldHolder>> recCache = new ConcurrentDictionary<Type, Dictionary<string, FieldHolder>>();
 
         private abstract class FieldHolder {
             abstract public void set(object rec, object value);
@@ -72,21 +73,16 @@ namespace OutSystems.NssDataGridUtils {
         }
 
         private static Dictionary<string, FieldHolder> getFields(Type leClass) {
-            if (recCache.ContainsKey(leClass))
-                return recCache[leClass];
-
-            Dictionary<string, FieldHolder> fields = new Dictionary<string, FieldHolder>();
-            foreach (var field in leClass.GetFields().Where(f => f.Name.StartsWith("ss"))) {
-                fields.Add(field.Name.Substring(2).ToLower(), new FieldField(field));
-            }
-
-            foreach (var prop in leClass.GetProperties().Where(p => p.Name.StartsWith("ss"))) {
-                fields.Add(prop.Name.Substring(2).ToLower(), new PropField(prop));
-            }
-
-            recCache[leClass] = fields;
-
-            return fields;
+            return recCache.GetOrAdd(leClass, t => {
+                var fields = new Dictionary<string, FieldHolder>();
+                foreach (var field in t.GetFields().Where(f => f.Name.StartsWith("ss"))) {
+                    fields.Add(field.Name.Substring(2).ToLower(), new FieldField(field));
+                }
+                foreach (var prop in t.GetProperties().Where(p => p.Name.StartsWith("ss"))) {
+                    fields.Add(prop.Name.Substring(2).ToLower(), new PropField(prop));
+                }
+                return fields;
+            });
         }
 
         private static void writeRecord(JsonWriter json, object rec, int dateFormat) {
@@ -113,12 +109,8 @@ namespace OutSystems.NssDataGridUtils {
                 }
             } catch { }
 
-            // structures and its attributes
             foreach (var field in leClass.GetFields().Where(f => f.Name.StartsWith("ss"))) {
-                // we must check if field is Record or RecordList, if they are, we remove ssEN (ssEntity) or ssST (ssStructure); 
-                clearStart = typeof(IRecord).IsAssignableFrom(field.FieldType) || typeof(ISimpleRecord).IsAssignableFrom(field.FieldType) ? 
-                    field.Name.StartsWith("ssEN") || field.Name.StartsWith("ssST") :
-                    false;
+                clearStart = field.Name.StartsWith("ssEN") || field.Name.StartsWith("ssST");
                 //RGRIDT-364 - removing columns of the type BinaryData.
                 if (typeof(Byte[]).IsAssignableFrom(field.FieldType) == false) {
                     json.WritePropertyName(field.Name.Substring(clearStart ? 4 : 2));
@@ -126,7 +118,6 @@ namespace OutSystems.NssDataGridUtils {
                 }
             }
 
-            // entities and its attributes
             foreach (var property in leClass.GetProperties().Where(p => p.Name.StartsWith("ss"))) {
                 //RGRIDT-364 - removing columns of the type BinaryData.
                 if (typeof(Byte[]).IsAssignableFrom(property.PropertyType) == false) {
@@ -168,14 +159,14 @@ namespace OutSystems.NssDataGridUtils {
                     }
                     else
                     {
+                        //Add dates from the ArrangeData action should be returned in UTC
+                        dv = dv.ToUniversalTime();
                         if (dv.Hour == 0 && dv.Minute == 0 && dv.Second == 0) // extra milisecond check ?
                         {
                             json.WriteValue(dv.ToString("yyyy-MM-dd"));
                         }
                         else
                         {
-                            //Add dates from the ArrangeData action should be returned in UTC
-                            dv = dv.ToUniversalTime();
                             json.WriteValue(dv.ToString("yyyy-MM-dd'T'HH:mm:ssZ"));
                         }
                     }
